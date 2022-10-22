5cb03dd144eb74bbdc7a46ddca20c5655a1d3a0b
Walyson Moura <walysonmoura222@gmail.com>
2022-10-22 02:49:14 -0300

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,13 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle, theme } from "../styles/globals"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle,theme } from '../styles/globals'
 
-export default function App({Component,pageProps }) {
-  return(
+export default function App({ Component, pageProps }) {
+  return (
     <>
-      <GlobalStyle>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps}/> 
-        </ThemeProvider>
-      </GlobalStyle>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
